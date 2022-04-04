4392522474846ad69897cc3934fb3b5204dca777
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-04 23:01:19 +0600

    About page added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='m-20 p-10 rounded-md shadow-xl w-2/5 mx-auto'>
+            <h1 className='text-violet-400 font-semibold text-4xl'>About</h1>
+            <p>This Website is about reviewing latest gadgets, gears and smart technologies.We discuss about the band new features added to the products and show user reviews to the clients. You can check user reviews of the product in the Reviews page. If you want you product to be reviewed and get user feedback ,then contact us or email us.And alway remember,
+                <br></br>
+                "This is the future"</p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -5,10 +5,11 @@ const Blogs = () => {
         <div>
             <div className='m-10 p-10 rounded-md shadow-md'>
                 <h1>Q1: What is context api</h1>
-                <p></p>
+                <p>Context api is a way to send props from components to components without using props drilling. We can send variables ,functions as well as values to any components. People use context api for state managements.The content one wants to share to other components context should be declare in that parent component. Context are also used in changing to dark theme ,light theme changing , showing the website in different languages.</p>
             </div>
             <div className='m-10 p-10 rounded-md shadow-md'>
                 <h1>Q2:What is Semantic tag</h1>
+                <p>Semantag are html tags with meaning or to be recognized easily.Other developer and search engines can crawl the website and collect data easily through semantic tags. Sematics tags organises the content of the website.A user can easily navigate .We </p>
             </div>
             <div className='m-10 p-10 rounded-md shadow-md'>
                 <h1>Q3:Difference betweenInline Block and Inline Block Elements</h1>
