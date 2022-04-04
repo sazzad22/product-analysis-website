ba215d3f55ae12897a932ffe88e48154724defd5
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-04 15:29:42 +0600

    Blogs page

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>Blogs</h1>
+            <div className='m-10 p-10 rounded-md shadow-md'>
+                <h1>Q1: What is context api</h1>
+                <p></p>
+            </div>
+            <div className='m-10 p-10 rounded-md shadow-md'>
+                <h1>Q2:What is Semantic tag</h1>
+            </div>
+            <div className='m-10 p-10 rounded-md shadow-md'>
+                <h1>Q3:Difference betweenInline Block and Inline Block Elements</h1>
+                <p></p>
+            </div>
         </div>
     );
 };
